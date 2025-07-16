8e7a5ca554445da67309f0aa54b59eedfff19b6a
d-dcarl <47509902+d-dcarl@users.noreply.github.com>
2025-07-15 20:39:51 -0600

    Revert "index and vite update"
    This reverts commit 0443b4ae893d59cb95ec59c6eecd7a17cdf1ba69.

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     // Base public path when served in production
-    base: '/d-dcarl.github.io/',
+    base: process.env.NODE_ENV === 'production' ? '/d-dcarl.github.io/' : '/',
 
     // Configure the server
     server: {
